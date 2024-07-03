f4be1e63f78957735a5a7d4d379676dd139f0b14
Maryam-Elmorshidy <engotta2002@gmail.com>
2024-07-03 12:20:14 +0300

    home page

---
+++ home.py
@@ -0,0 +1,29 @@
+import streamlit as st 
+from st_pages import Page, Section, show_pages, add_page_title, hide_pages
+
+# # Read the CSS file
+# with open('style/style_css.css') as f:
+#     css = f.read()
+
+# # Inject the CSS into the Streamlit app
+# st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+
+st.title("فقه النفس")
+
+# st.markdown(
+#     """
+#     <link href="//db.onlinewebfonts.com/c/6b75b24d502dab23003320c2e1b2fc68?family=Adobe+Arabic" rel="stylesheet" type="text/css"/>
+#     <style> bdi {font-family: 'Adobe Arabic';}</style>
+
+#     <p>The Arabic word for "website" is <bdi>موقع الكتروني</bdi></p>
+#     """,
+#     unsafe_allow_html=True,
+# )
+
+# st.markdown(
+#     """
+#    موقع الكتروني
+#     """,
+#     unsafe_allow_html=True,
+# )
